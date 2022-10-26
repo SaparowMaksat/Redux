efb0838741743d7b4913a65c5aa447ca98bd15ae
Max07 <102533562+SaparowMaksat@users.noreply.github.com>
2022-10-26 05:16:34 +0300

    Redux

---
+++ src/store/actions.js
@@ -0,0 +1,19 @@
+import * as actionsTypes from './actionTypes'
+
+export function taskCompleted(id) {
+	return {
+		type: actionsTypes.taskUpdated,
+		payload: { id, completed: true },
+	}
+}
+
+export function titleChange(id) {
+	return {
+		type: actionsTypes.taskUpdated,
+		payload: { id, title: `New title for ${id}` },
+	}
+}
+
+export function taskDeleted(id) {
+	return { type: actionsTypes.taskDeleted, payload: { id } }
+}
